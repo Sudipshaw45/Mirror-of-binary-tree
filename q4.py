fd580caf2b240982c555b10889850e989caa020b
Sudipshaw45 <sudipsh2006@gmail.com>
2025-09-10 15:23:34 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, val):  
+        self.data = val
+        self.right = None
+
+class Solution:
+    def mirror(self, root):
+        if root is None:
+            return
+      
+        root.left, root.right = root.right, root.left
+        
+        self.mirror(root.left)
+        self.mirror(root.right)
